1a1cc029c29b2952474821eb3eeeaf8cdfac14ea
Jan Gillich <jan.gillich@tink.se>
2020-02-14 15:34:01 +0100

    ref(models): Make Period model more sophisticated

--- models/src/main/java/com/tink/model/time/Period.kt
+++ models/src/main/java/com/tink/model/time/Period.kt
@@ -4,12 +4,41 @@ import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 import org.threeten.bp.Instant
 
+sealed class Period : Parcelable {
+    abstract val start: Instant
+    abstract val end: Instant
+}
+
 @Parcelize
-data class Period(
-    val day: Int?,
-    val week: Int?,
-    val month: Int?,
-    val year: Int?,
-    val start: Instant?,
-    val end: Instant?
-) : Parcelable
+data class YearPeriod(
+    val year: Int,
+    override val start: Instant,
+    override val end: Instant
+) : Period()
+
+@Parcelize
+data class MonthPeriod(
+    val monthOfYear: Int,
+    val year: Int,
+    override val start: Instant,
+    override val end: Instant
+) : Period() {
+    init {
+        require(monthOfYear in 1..12)
+    }
+}
+
+@Parcelize
+data class DayPeriod(
+    val dayOfMonth: Int,
+    val monthOfYear: Int,
+    val year: Int,
+    override val start: Instant,
+    override val end: Instant
+) : Period() {
+    init {
+        require(dayOfMonth in 1..31)
+        require(monthOfYear in 1..12)
+    }
+}
+
