848fc6b3dd5acbfce08f3fa00b88c382b1dd3d90
Jan Gillich <jan.gillich@tink.se>
2020-02-14 15:10:33 +0100

    feat(models): Parcelize category and category tree

--- models/src/main/java/com/tink/model/category/Category.kt
+++ models/src/main/java/com/tink/model/category/Category.kt
@@ -1,5 +1,9 @@
 package com.tink.model.category
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Category(
     val code: String,
     val id: String,
@@ -7,10 +11,11 @@ data class Category(
     val children: List<Category>,
     val sortOrder: Int,
     val isDefaultChild: Boolean
-)
+) : Parcelable
 
+@Parcelize
 data class CategoryTree(
     val expenses: Category,
     val income: Category,
     val transfers: Category
-)
+) : Parcelable
