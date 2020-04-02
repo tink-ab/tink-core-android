63668156e486826004efd3e9fc8bc894575a12f2
Jan Gillich <jan.gillich@tink.se>
2020-04-02 11:17:43 +0200

    sty(RetrofitModule): Remove unused import

--- service/src/main/java/com/tink/service/di/RetrofitModule.kt
+++ service/src/main/java/com/tink/service/di/RetrofitModule.kt
@@ -8,7 +8,6 @@ import dagger.Provides
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 class RetrofitModule {
