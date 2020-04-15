d40a4b3d896d588966755bbaf17d8642bc21ce97
Jan Gillich <jan.gillich@tink.se>
2020-04-15 18:09:34 +0200

    feat(credential service): Add refreshable items as a parameter
    for creation and refreshing

--- service/src/main/java/com/tink/service/credentials/CredentialDescriptors.kt
+++ service/src/main/java/com/tink/service/credentials/CredentialDescriptors.kt
@@ -2,12 +2,14 @@ package com.tink.service.credentials
 
 import android.net.Uri
 import com.tink.model.credentials.Credentials
+import com.tink.model.credentials.RefreshableItem
 
 data class CredentialsCreationDescriptor(
     val providerName: String,
     val type: Credentials.Type,
     val fields: Map<String, String>,
-    val appUri: Uri
+    val appUri: Uri,
+    val refreshableItems: Collection<RefreshableItem> = listOf()
 )
 
 data class CredentialsUpdateDescriptor(
@@ -15,4 +17,9 @@ data class CredentialsUpdateDescriptor(
     val providerName: String,
     val fields: Map<String, String>,
     val appUri: Uri
+)
+
+data class CredentialsRefreshDescriptor(
+    val id: String,
+    val refreshableItems: Collection<RefreshableItem> = listOf()
 )
