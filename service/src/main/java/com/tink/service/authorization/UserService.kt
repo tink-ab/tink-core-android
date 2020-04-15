649477101cd0042c905ee625b7e6156bb421e335
Magnus Gudmandsen <magnus.gudmandsen@tink.se>
2020-04-15 18:10:12 +0200

    refactor(scope): move Scope into models

--- service/src/main/java/com/tink/service/authorization/UserService.kt
+++ service/src/main/java/com/tink/service/authorization/UserService.kt
@@ -6,19 +6,6 @@ interface UserService {
     suspend fun createAnonymousUser(arguments: UserCreationDescriptor): String
 }
 
-sealed class Scope(private val scope: String) {
-    object TransactionsRead : Scope("transactions:read")
-    object AccountsRead : Scope("accounts:read")
-    object UserRead : Scope("user:read")
-    object CredentialsRead : Scope("credentials:read")
-    object IdentityRead : Scope("identity:read")
-    object InvestmentsRead : Scope("investments:read")
-    object StatisticsRead : Scope("statistics:read")
-    class CustomScope(scope: String) : Scope(scope)
-
-    override fun toString(): String = scope
-}
-
 data class UserCreationDescriptor(
     val market: String,
     val locale: String
