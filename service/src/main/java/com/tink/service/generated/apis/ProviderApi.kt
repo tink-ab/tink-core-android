ab1d2a40215b5a93868c83c4358ec16e4be00de3
Magnus Gudmandsen <magnus.gudmandsen@tink.se>
2020-04-15 17:49:02 +0200

    fix(provider-api): remove unused param from docs

--- service/src/main/java/com/tink/service/generated/apis/ProviderApi.kt
+++ service/src/main/java/com/tink/service/generated/apis/ProviderApi.kt
@@ -17,7 +17,6 @@ interface ProviderApi {
      * List providers
      * Lists all providers available for a authenticated user.
      * The endpoint is owned by defaultname service owner
-     * @param body (optional)
      * @param capability Use the capability to only list providers with a specific capability. If no capability the provider response will not be filtered on capability. (optional)
      * @param includeTestProviders Defaults to false. If set to &#x60;true&#x60;, Providers of &#x60;TEST&#x60; type will be added in the response list. (optional)
      * @param excludeNonTestProviders Defaults to false. If set to &#x60;true&#x60;, Providers of type different than &#x60;TEST&#x60; will be removed from the response list. (optional)
@@ -39,7 +38,6 @@ interface ProviderApi {
      * Lists all providers on a specified market. Your authentication affects the list of providers you get: When authenticated, the result will only contain providers available for that user. When unauthenticated and without passing The OAuth2 Client ID header, you get all providers made available for the entire environment. If you are unauthenticated but you include The OAuth2 Client ID header, you will get providers available for the app with the given client id.
      * The endpoint is owned by defaultname service owner
      * @param market The ISO 3166-1 alpha-2 market code. (required)
-     * @param body (optional)
      * @param xTinkOAuthClientID The OAuth2 Client ID (optional)
      * @param acceptLanguage Language to translate to. (optional)
      * @param includeTestProviders Defaults to false. If set to &#x60;true&#x60;, Providers of &#x60;TEST&#x60; type will be added in the response list. (optional)
@@ -63,7 +61,6 @@ interface ProviderApi {
      * List markets
      * Lists all markets where there are providers available.
      * The endpoint is owned by defaultname service owner
-     * @param body (optional)
      * @param xTinkOAuthClientID The OAuth2 Client ID (optional)
      */
     @Headers(
