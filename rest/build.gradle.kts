918b14e5c30149a124c7996f4deaba74ae0bcd70
Magnus Gudmandsen <magnus.gudmandsen@tink.se>
2020-04-17 11:22:51 +0200

    fix(rest): add dependencies

--- rest/build.gradle.kts
+++ rest/build.gradle.kts
@@ -1,7 +1,19 @@
 plugins {
     kotlin("jvm")
+    kotlin("kapt")
 }
 
 dependencies {
     implementation(Dependencies.kotlin_stdlib)
+
+    implementation(Dependencies.Moshi.moshi)
+    implementation(Dependencies.Moshi.moshi_adapters)
+    implementation(Dependencies.Moshi.moshi_kotlin)
+    kapt(Dependencies.Moshi.moshi_codegen)
+
+    implementation(Dependencies.Retrofit.retrofit)
+    implementation(Dependencies.Retrofit.retrofit_converter_moshi)
+
+    implementation(Dependencies.three_ten_abp)
+    implementation(Dependencies.okhttp)
 }
