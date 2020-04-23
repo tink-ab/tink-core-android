2dfd8f6050061bb43dced480b0b25e4d38b571b7
Jan Gillich <jan.gillich@tink.se>
2020-04-23 17:40:33 +0200

    feat(account service rest): Add converters

---
+++ service/src/main/java/com/tink/service/account/AccountConverters.kt
@@ -0,0 +1,38 @@
+package com.tink.service.account
+
+import com.tink.model.account.Account
+import com.tink.service.misc.toAmount
+import com.tink.service.misc.toExactNumber
+import com.tink.rest.models.Account as AccountDTO
+import com.tink.rest.models.Account.TypeEnum as AccountTypeDTO
+
+
+fun AccountDTO.toCoreModel(): Account =
+    Account(
+        id = id,
+        accountNumber = accountNumber,
+        balance = currencyDenominatedBalance?.toAmount()!!,
+        credentialsId = credentialsId,
+        excluded = excluded,
+        favored = favored,
+        transactional = false, //TODO
+        closed = closed ?: false,
+        name = name,
+        ownership = ownership.toExactNumber(),
+        type = type.toCoreModel(),
+        images = null //TODO
+    )
+
+
+fun AccountTypeDTO.toCoreModel() =
+    when (this) {
+        AccountTypeDTO.CHECKING -> Account.Type.TYPE_CHECKING
+        AccountTypeDTO.SAVINGS -> Account.Type.TYPE_SAVINGS
+        AccountTypeDTO.INVESTMENT -> Account.Type.TYPE_INVESTMENT
+        AccountTypeDTO.MORTGAGE -> Account.Type.TYPE_MORTGAGE
+        AccountTypeDTO.CREDIT_CARD -> Account.Type.TYPE_CREDIT_CARD
+        AccountTypeDTO.LOAN -> Account.Type.TYPE_LOAN
+        AccountTypeDTO.PENSION -> Account.Type.TYPE_PENSION
+        AccountTypeDTO.OTHER -> Account.Type.TYPE_OTHER
+        AccountTypeDTO.EXTERNAL -> Account.Type.TYPE_EXTERNAL
+    }
---
+++ service/src/main/java/com/tink/service/misc/NumberConverters.kt
@@ -0,0 +1,10 @@
+package com.tink.service.misc
+
+import com.tink.model.misc.ExactNumber
+import java.math.BigDecimal
+
+internal fun Double.toExactNumber() =
+    BigDecimal(this).toExactNumber()
+
+internal fun BigDecimal.toExactNumber() =
+    ExactNumber(unscaledValue().toLong(), scale().toLong())
