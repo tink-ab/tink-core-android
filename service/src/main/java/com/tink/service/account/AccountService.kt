5e8d8eb74da44555c93025f3d6f40f7c6f1b8549
Jan Gillich <jan.gillich@tink.se>
2020-04-23 15:19:07 +0200

    feat(account service rest): Use suspend functions for list/update

--- service/src/main/java/com/tink/service/account/AccountService.kt
+++ service/src/main/java/com/tink/service/account/AccountService.kt
@@ -2,8 +2,30 @@ package com.tink.service.account
 
 import com.tink.model.account.Account
 import com.tink.service.handler.ResultHandler
+import com.tink.service.observer.ChangeObserver
+import javax.inject.Inject
 
 interface AccountService {
+    fun subscribe(listener: ChangeObserver<List<Account>>)
+    fun unsubscribe(listener: ChangeObserver<List<Account>>)
+    suspend fun listAccounts(): List<Account>
+    suspend fun update(account: UpdateAccountDescriptor): Account
+}
 
-    fun update(descriptor: UpdateAccountDescriptor, handler: ResultHandler<Account>)
+class AccountServiceImpl @Inject constructor() : AccountService {
+    override fun subscribe(listener: ChangeObserver<List<Account>>) {
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun unsubscribe(listener: ChangeObserver<List<Account>>) {
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
+    }
+
+    override suspend fun listAccounts(): List<Account> {
+        TODO("Not yet implemented")
+    }
+
+    override suspend fun update(account: UpdateAccountDescriptor): Account {
+        TODO("Not yet implemented")
+    }
 }
