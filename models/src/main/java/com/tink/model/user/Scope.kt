3541dd98b93dfb3e0f4ff5df399e8c594bfdf4be
Mohan Kumar <mohan.kumar@tink.se>
2020-04-28 16:04:15 +0200

    refactor(scope): Made Scope model class parcelable

--- models/src/main/java/com/tink/model/user/Scope.kt
+++ models/src/main/java/com/tink/model/user/Scope.kt
@@ -1,14 +1,33 @@
 package com.tink.model.user
 
-sealed class Scope(private val scope: String) {
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+sealed class Scope(private val scope: String): Parcelable {
+
+    @Parcelize
     object TransactionsRead : Scope("transactions:read")
+
+    @Parcelize
     object AccountsRead : Scope("accounts:read")
+
+    @Parcelize
     object UserRead : Scope("user:read")
+
+    @Parcelize
     object CredentialsRead : Scope("credentials:read")
+
+    @Parcelize
     object IdentityRead : Scope("identity:read")
+
+    @Parcelize
     object InvestmentsRead : Scope("investments:read")
+
+    @Parcelize
     object StatisticsRead : Scope("statistics:read")
-    class CustomScope(scope: String) : Scope(scope)
+
+    @Parcelize
+    class CustomScope(private val scope: String) : Scope(scope)
 
     override fun toString(): String = scope
 }
