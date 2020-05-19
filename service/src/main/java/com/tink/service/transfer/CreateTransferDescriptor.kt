fb25d7795b63cf8eea9c8d5517ed1e5392ade72e
Jan Gillich <jan.gillich@tink.se>
2020-05-19 14:36:31 +0200

    feat(transfer): Make sourceMessage nullable

--- service/src/main/java/com/tink/service/transfer/CreateTransferDescriptor.kt
+++ service/src/main/java/com/tink/service/transfer/CreateTransferDescriptor.kt
@@ -5,7 +5,7 @@ import com.tink.model.misc.Amount
 data class CreateTransferDescriptor(
     val amount: Amount,
     val sourceUri: String,
-    val sourceMessage: String,
+    val sourceMessage: String?,
     val destinationUri: String,
     val destinationMessage: String,
     val credentialsId: String? = null
