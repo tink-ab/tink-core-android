9ad73dfc7ddca215078276f8d76d938a69544d9c
Magnus Gudmandsen <maggud@users.noreply.github.com>
2020-06-01 12:37:27 +0200

    Release 0.4.0

--- models/src/main/java/com/tink/model/misc/ExactNumber.kt
+++ models/src/main/java/com/tink/model/misc/ExactNumber.kt
@@ -10,6 +10,13 @@ data class ExactNumber(
     val scale: Long
 ) : Comparable<ExactNumber>, Parcelable {
 
+    constructor(double: Double) : this(BigDecimal.valueOf(double))
+    constructor(long: Long) : this(long, 0)
+    constructor(bigDecimal: BigDecimal) : this(
+        bigDecimal.unscaledValue().toLong(),
+        bigDecimal.scale().toLong()
+    )
+
     fun toBigDecimal() = BigDecimal(unscaledValue.toBigInteger(), scale.toInt())
 
     override fun compareTo(other: ExactNumber): Int = toBigDecimal().compareTo(other.toBigDecimal())
---
+++ models/src/main/java/com/tink/model/transfer/Beneficiary.kt
@@ -0,0 +1,29 @@
+package com.tink.model.transfer
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+import com.tink.model.account.Account
+import kotlinx.android.parcel.IgnoredOnParcel
+
+/**
+ * A beneficiary of a transfer
+ *
+ * @param accountId The id of the source account this beneficiary belongs to, see [Account.id]
+ * @param accountNumber The account number of the beneficiary, for example the BG/PG number or the IBAN
+ * @param name The name of the beneficiary
+ * @param type The type of transfer that is used for this beneficiary, for example "iban"
+ */
+@Parcelize
+data class Beneficiary(
+    val ownerAccountId: String,
+    val accountNumber: String,
+    val accountNumberType: String,
+    val name: String
+) : Parcelable {
+
+    /*
+     * The uri which can be used to make a transfer to this beneficiary
+     */
+    @IgnoredOnParcel
+    val uri: String = "$accountNumberType://$accountNumber"
+}
