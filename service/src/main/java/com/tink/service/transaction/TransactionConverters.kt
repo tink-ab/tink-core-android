5ac246859de4772ac0a077220efdfca43facd474
Jan Gillich <jan.gillich@tink.se>
2020-07-17 17:43:41 +0200

    feat(pfm-core): Add more transaction features

--- service/src/main/java/com/tink/service/transaction/TransactionConverters.kt
+++ service/src/main/java/com/tink/service/transaction/TransactionConverters.kt
@@ -1,13 +1,13 @@
 package com.tink.service.transaction
 
 import com.tink.model.transaction.Transaction
-import com.tink.model.transaction.TransactionDetails
+import com.tink.rest.models.TransactionResponse
 import com.tink.service.misc.toAmount
 import com.tink.service.misc.toInstant
-import com.tink.rest.models.TransactionResponse as TransactionDto
+import com.tink.rest.models.Transaction as TransactionDto
 
-fun TransactionDto.toCoreModel(): Transaction {
-    return Transaction(
+fun TransactionDto.toCoreModel() =
+    Transaction(
         accountId = accountId,
         id = id,
         amount = currencyDenominatedAmount!!.toAmount(),
@@ -25,4 +25,24 @@ fun TransactionDto.toCoreModel(): Transaction {
         inserted = 0L.toInstant(), //TODO
         details = null
     )
-}
+
+fun TransactionResponse.toCoreModel() =
+    Transaction(
+        accountId = accountId,
+        id = id,
+        amount = currencyDenominatedAmount!!.toAmount(),
+        dispensableAmount = currencyDenominatedAmount!!.toAmount(), //TODO
+        description = description,
+        categoryCode = categoryId,
+        date = date.toInstant(),
+        notes = notes,
+        tags = listOf(),
+        upcoming = upcoming,
+        pending = pending,
+        originalDate = originalDate.toInstant(),
+        originalDescription = originalDescription,
+        originalAmount = currencyDenominatedOriginalAmount!!.toAmount(),
+        inserted = 0L.toInstant(), //TODO
+        details = null
+    )
+
--- service/src/main/java/com/tink/service/transaction/TransactionServiceImpl.kt
+++ service/src/main/java/com/tink/service/transaction/TransactionServiceImpl.kt
@@ -3,6 +3,9 @@ package com.tink.service.transaction
 import com.tink.model.time.Period
 import com.tink.model.transaction.Transaction
 import com.tink.rest.apis.SearchApi
+import com.tink.rest.apis.TransactionApi
+import com.tink.rest.models.CategorizeTransactionsListRequest
+import com.tink.rest.models.CategorizeTransactionsRequest
 import com.tink.rest.models.SearchQuery
 import javax.inject.Inject
 
@@ -11,11 +14,21 @@ interface TransactionService {
         accountId: String? = null,
         categoryCode: String? = null,
         period: Period? = null
-    ): List<Transaction>
+    ): List<Transaction> //TODO: Paging
+
+    suspend fun getTransaction(transactionId: String): Transaction
+
+    suspend fun categorizeTransactions(
+        transactionIds: List<String>,
+        categoryCode: String
+    ) // TODO: Should return transactions?
+
+    suspend fun getSimilarTransactions(transactionId: String): List<Transaction>
 }
 
 internal class TransactionServiceImpl @Inject constructor(
-    private val api: SearchApi
+    private val searchApi: SearchApi,
+    private val transactionApi: TransactionApi
 ) : TransactionService {
     override suspend fun listTransactions(
         accountId: String?,
@@ -35,6 +48,24 @@ internal class TransactionServiceImpl @Inject constructor(
             query.endDate = it.end.toEpochMilli()
         }
 
-        return api.searchQuery(query).results.mapNotNull { it.transaction?.toCoreModel() }
+        return searchApi.searchQuery(query).results.mapNotNull { it.transaction?.toCoreModel() }
     }
+
+    override suspend fun getTransaction(transactionId: String) =
+        transactionApi.getTransaction(transactionId).toCoreModel()
+
+    override suspend fun categorizeTransactions(
+        transactionIds: List<String>,
+        categoryCode: String
+    ) {
+        val request = CategorizeTransactionsListRequest(
+            listOf(
+                CategorizeTransactionsRequest(categoryCode, transactionIds) //TODO: code vs id?
+            )
+        )
+        transactionApi.categorize(request)
+    }
+
+    override suspend fun getSimilarTransactions(transactionId: String) =
+        transactionApi.similar(transactionId).transactions.map { it.toCoreModel() }
 }
