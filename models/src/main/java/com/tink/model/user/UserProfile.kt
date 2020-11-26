fdc2fa35abcda190c954a2aba32d5bff2d7ed525
Mohan Kumar Gopalakrishnan <mohan.kumar@tink.se>
2020-11-26 14:09:39 +0100

    Release 0.5.5

--- models/src/main/java/com/tink/model/user/UserProfile.kt
+++ models/src/main/java/com/tink/model/user/UserProfile.kt
@@ -1,21 +1,21 @@
 package com.tink.model.user
 
 import android.os.Parcelable
-import com.tink.model.authentication.AuthenticationMethod
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class UserProfile(
-    val username: String,
     val locale: String,
     val market: String,
     val timeZone: String,
     val currency: String,
-    val authorizedLoginMethods: Set<AuthenticationMethod>,
-    val availableAuthenticationMethods: Set<AuthenticationMethod>
+    val periodAdjustedDay: Int,
+    val periodMode: PeriodMode
 ) : Parcelable {
-    fun hasPassword(): Boolean {
-        return authorizedLoginMethods
-            .contains(AuthenticationMethod.EMAIL_AND_PASSWORD)
+
+    enum class PeriodMode {
+        UNKNOWN,
+        MONTHLY,
+        MONTHLY_ADJUSTED
     }
 }
