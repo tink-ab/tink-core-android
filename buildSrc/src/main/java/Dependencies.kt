9330f7a1fde2e18985763ad994f0874a8d323f70
Oskar Höjwall <oskar.hojwall@tink.com>
2021-03-30 15:15:51 +0200

    Update dependencies

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,25 +1,25 @@
 object Versions {
-    const val compileSdk = 29
-    const val buildTools = "29.0.1"
+    const val compileSdk = 30
+    const val buildTools = "30.0.3"
     const val minSdk = 21
-    const val targetSdk = 29
+    const val targetSdk = 30
 
     const val conscrypt = "2.4.0"
     const val coroutines = "1.4.2"
-    const val dagger = "2.16"
+    const val dagger = "2.24"
     const val kotlin = "1.4.10"
     const val moshi = "1.11.0"
     const val okhttp = "4.4.0"
-    const val pinterestktlint = "0.33.0"
+    const val pinterestktlint = "0.41.0"
     const val retrofit = "2.6.2"
     const val rxjava = "2.1.9"
-    const val threetenabp = "1.2.1"
+    const val threetenabp = "1.3.0"
     const val timber = "4.7.1"
 
     // test
-    const val assertj = "3.11.1"
+    const val assertj = "3.19.0"
     const val junit = "5.6.2"
-    const val mockk = "1.8.7"
+    const val mockk = "1.11.0"
 }
 
 object Dependencies {
