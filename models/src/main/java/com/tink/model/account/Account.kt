23bd699bcd601fe1a8d24612afbe829f14aec353
Oskar Höjwall <oskar.hojwall@tink.com>
2021-09-27 09:46:28 +0200

    Added first seen property

--- models/src/main/java/com/tink/model/account/Account.kt
+++ models/src/main/java/com/tink/model/account/Account.kt
@@ -25,7 +25,8 @@ data class Account(
     val refreshed: Instant = Instant.EPOCH,
     val financialInstitutionID: String? = null,
     val identifiers: List<String>,
-    val transferDestinations: List<String>
+    val transferDestinations: List<String>,
+    val firstSeen: Instant? = null
 ) : Parcelable {
 
     enum class Type {
