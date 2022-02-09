c20362fae784a11547c92899d98c58cc06b07fc3
Jonathan Persgården <jonathan.persgarden@tink.com>
2022-02-09 09:58:56 +0100

    Added map function to Response state (#230)
    Added map function to Response state

--- service/src/main/java/com/tink/service/network/ResponseState.kt
+++ service/src/main/java/com/tink/service/network/ResponseState.kt
@@ -9,3 +9,10 @@ data class SuccessState<T>(val data: T) : ResponseState<T>()
 data class ErrorState<T>(val errorMessage: String? = null) : ResponseState<T>() {
     constructor(throwable: Throwable) : this(throwable.message)
 }
+
+fun <T, R> ResponseState<T>.map(f: (T) -> R): ResponseState<R> =
+    when (this) {
+        is SuccessState -> SuccessState(f(data))
+        is LoadingState -> LoadingState
+        is ErrorState -> ErrorState(errorMessage)
+    }
