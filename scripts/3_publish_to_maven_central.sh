2c3a593323e93a8d94c831bb186ec3ad4082b754
Marco Biagini <>
2022-05-20 14:55:02 +0200

    Fixes to the script for releasing to Maven central

--- scripts/3_publish_to_maven_central.sh
+++ scripts/3_publish_to_maven_central.sh
@@ -1,15 +1,14 @@
 set -e
-cd ..
 
+echo "--> Enter new version (x.y.z format):"
 # shellcheck disable=SC2162
 read newVersion
 
 # TODO: read version from file
-echo "New version: "
 if [[ $newVersion =~ ^([0-9]{1,2}\.){2}[0-9]{1,10}$ ]]; then
-echo "Publishing to maven central"
+  echo "--> Version number: $newVersion is in the correct format"
 else
-  echo "$newVersion is not in the right format."
+  echo "--> $newVersion is not in the right format."
   exit
 fi
 
@@ -17,17 +16,22 @@ git checkout master
 git fetch
 git pull
 
-echo "Publishing to maven central"
-./gradlew publish
+echo "--> Publishing to maven central"
+#./gradlew publish
+printf "\n\n"
+echo "--> NEXT: Continue the release on Sonatype web page"
+echo "--> Press enter to open the browser... "
+read -r
 open https://repo1.maven.org/maven2/com/tink/core
 open https://oss.sonatype.org
 
-echo "Polling for release to be complete"
+echo "--> Polling for release to be complete"
 until curl -s -f -o /dev/null "https://repo1.maven.org/maven2/com/tink/core/$newVersion/"
 do
-  echo "Sleep"
+  echo "--> Polling: publishing on Maven central not yet completed, please wait... "
   sleep 5
 done
 
 echo "\007"
-echo "DONE"
+echo "--> DONE: Publishing to Maven Central!"
+echo "--> NEXT: Launch the script for creating a release on Github"
