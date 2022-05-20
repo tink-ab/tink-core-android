904fa0ae217338f19a1ea31f2b6022a9c667c099
Marco Biagini <>
2022-05-20 13:57:19 +0200

    update update-version script

--- scripts/updateVersion.sh
+++ scripts/updateVersion.sh
@@ -1,25 +1,44 @@
 set -e
-cd ..
 
-# Should be given a version on the format x.y.z as argument
-echo "New version: "
+echo "--> Running ktlintFormat"
+./gradlew ktlintFormat
+
+if [[ $(git status --porcelain) ]]; then
+  echo "--> There are changes to commit/discard before you can continue with the release"
+  exit
+else
+ echo "--> No changes to commit/discard, continuing with the release"
+fi
+
+echo "--> Enter new version (x.y.z format):"
 # shellcheck disable=SC2162
 read newVersion
+echo "--> Dry-run? (y/n):"
+# shellcheck disable=SC2162
+read isDryRun
 
 if [[ $newVersion =~ ^([0-9]{1,2}\.){2}[0-9]{1,10}$ ]]; then
-echo "Starting release with version: $newVersion"
+echo "--> Version number: $newVersion is in the correct format"
 else
-  echo "$newVersion is not in the right format."
+  echo "--> $newVersion is not in the right format."
   exit
 fi
 
 branchName=version-bump-"$newVersion"
+if [[ $isDryRun = 'y' ]]
+then
+  masterBranch=master-dry-run-"$newVersion"
+else
+  masterBranch=master
+fi
 
+echo "--> Creating branch $branchName"
 git checkout development
 git fetch
 git pull
 git checkout -b "$branchName"
 
+echo "--> Updating Version.kt file"
 major=$(echo "$newVersion" | cut -d. -f1)
 minor=$(echo "$newVersion" | cut -d. -f2)
 patch=$(echo "$newVersion" | cut -d. -f3)
@@ -33,7 +52,7 @@ echo "object Version {
     private const val minor = $minor
     private const val patch = $patch
 
-    const val name = \"\$major.\$minor.\$patch\"
+    const val name = \"$major.$minor.$patch\"
 
     private const val minorOffset = 100 //make space for 100 patches per minor version
     private const val majorOffset = 100 * minorOffset //make space for 100 minor versions per major version
@@ -43,11 +62,25 @@ echo "object Version {
     const val code = major * majorOffset + minor * minorOffset + patch
 }
 " >> $versionFilePath
-
 git add $versionFilePath
+
+echo "--> Creating version bump commit"
 git commit -m "Version bump to $newVersion"
 git push --set-upstream origin "$branchName"
 
-echo "Version bumped to $newVersion!"
-echo "Merge $branchName to development and then development to master"
-echo "Then launch the pre-release.sh script"
+printf "\n\n"
+echo "--> DONE: Version bumped to $newVersion!"
+if [[ $isDryRun = 'y' ]]
+then
+  git checkout master
+  git pull
+  git checkout -b $masterBranch
+  git merge "$branchName" -m "Dry-run merge $branchName into $masterBranch"
+  git push --set-upstream origin $masterBranch
+  printf "\n\n"
+  echo "--> NEXT: Launch the pre-release.sh script in dry-run mode"
+else
+  echo "--> NEXT: 1) Open Prs to merge $branchName to development and then development to master"
+  echo "--> NEXT: 2) Launch the pre-release.sh script"
+fi
+printf "\n\n"
