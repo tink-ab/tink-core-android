a6e24b24e51266c45f5fcf59c60deece767e58e5
Marco Biagini <>
2022-05-25 13:52:19 +0200

    Update to Kotlin 1.6.21

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -7,7 +7,7 @@ object Versions {
     const val conscrypt = "2.5.2"
     const val coroutines = "1.5.2"
     const val dagger = "2.40.5"
-    const val kotlin = "1.5.31"
+    const val kotlin = "1.6.21"
     const val moshi = "1.12.0"
     const val okhttp = "4.9.2"
     const val pinterestktlint = "0.45.1"
